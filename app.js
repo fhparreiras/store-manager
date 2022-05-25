a7e04c22cf216555a1ed27ced23d42d52ee6bc72
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-05-25 15:33:37 -0300

    chore:configuração diretório de rotas
    criando um diretório para as rotas

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/', require('./routes'));
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
@@ -9,5 +13,5 @@ app.get('/', (_request, response) => {
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
