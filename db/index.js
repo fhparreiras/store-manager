1ab24c7dd6abeef41c4eeabc530cf4f386dd7896
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-05-27 22:41:12 -0300

    Tentando req 2 novamente

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // Criando conexão com o banco de dados
 const connection = mysql.createPool({
-    host: process.env.MYSQL_USER || 'localhost', // usa process.env para Docker
+    host: process.env.MYSQL_HOST || 'localhost', // usa process.env para Docker
     port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'tryber',
     password: process.env.MYSQL_PASSWORD || '123horta',
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app } = require('./app');
+const app = require('./app');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
